eacf0ffdb1c45bdbc0ef0753d9826ac57632b548
Srishti Ahirwar <ahirwar.s@northeastern.edu>
2024-10-19 17:12:51 -0400

    Changed to v2

--- app.js
+++ app.js
@@ -7,17 +7,14 @@ const errorHandler = require("./middleware/errorHandler");
 const allowedMethods = require("./middleware/allowedMethods");
 const setHeaders = require("./middleware/setHeaders");
 
-
-
 const app = express();
-
 
 app.use(setHeaders);
 app.use(express.json());
 
 app.use(/^\/healthz$/, allowedMethods("GET"), healthCheck);
-app.use("/v1/user", allowedMethods("GET", "PUT", "POST"), userRoutes);
-app.use("/authenticated", allowedMethods( "POST"), authRoutes);
+app.use("/v2/user", allowedMethods("GET", "PUT", "POST"), userRoutes);
+app.use("/authenticated", allowedMethods("POST"), authRoutes);
 
 // Default response for all other paths
 app.all("*", async (req, res, next) => {
