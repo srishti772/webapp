dc4e9dac40713a7020ef111aa8e8832f1b43992b
Srishti Ahirwar <ahirwar.s@northeastern.edu>
2024-10-23 02:25:13 -0400

    Changed variable name of AMI_SOURCE_DESCRIPTION

--- packer/scripts/installSoftware.sh
+++ packer/scripts/installSoftware.sh
@@ -2,19 +2,6 @@ set -e
 export DEBIAN_FRONTEND="noninteractive"
 
 
-# Echo the values of environment variables
-echo "========================================"
-echo "Environment Variables"
-echo "----------------------------------------"
-echo "NEW_USER: ${NEW_USER}"
-echo "NEW_PASSWORD: ${NEW_PASSWORD}"
-echo "MYSQL_HOST: ${MYSQL_HOST}"
-echo "MYSQL_PORT: ${MYSQL_PORT}"
-echo "APP_PORT: ${PORT}"
-echo "ROOT_PASSWORD: $ROOT_PASSWORD"
-echo "MYSQL_DATABASE_TEST: $MYSQL_DATABASE_TEST"
-echo "MYSQL_DATABASE_PROD: $MYSQL_DATABASE_PROD"
-echo "========================================"
 
 # Install Unzip
 echo "Installing unzip..."
